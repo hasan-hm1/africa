b66384d33f53b8b871f4422e84a546dbd9b44649
Hasan Mohammed <hasanmohammed.hm1@gmail.com>
2024-04-06 00:38:05 +0300

    add preject resources

---
+++ Africa/Assets.xcassets/Africa/AfricaApp.dataset/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Hasan on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Africa/Screens/VideoListView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Hasan on 4/6/24.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoListView()
+}
